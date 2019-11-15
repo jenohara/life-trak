84caa03b8077b7b87288ef74b124c99788f16b4e
LadyWitch <jenny.s.ohara@gmail.com>
2019-11-15 18:46:46 +0000

    added index, show, new and create to user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,22 @@
 class UsersController < ApplicationController
+    def index
+        @users = User.all
+    end
+
+    def show
+        @user = User.find(params[:id])
+    end
+
+    def new
+    end
+
+    def create
+        @user = User.new
+        @user[:username] = params[:user][:username]
+        @user[:email] = params[:user][:email]
+        @user[:password] = params[:user][:password]
+        @user.save
+        redirect_to user_path(@user)
+    end
 end
+
