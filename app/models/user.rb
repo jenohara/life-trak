075632f4e3ec7ee8009997248775b9f6c7f40633
jenohara <jenny.s.ohara@gmail.com>
2020-01-14 15:02:58 +0000

    getting confused my devise

--- app/models/user.rb
+++
@@ -1,7 +0,0 @@
-class User < ApplicationRecord
-    has_secure_password
-
-    validates :username, :email, presence: true
-    validates :username, :email, uniqueness: true
-
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  devise_for :users
   root 'welcome#home'
   get 'welcome/home' => 'welcome#home'
 
