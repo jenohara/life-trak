d7a6ff8fab76c94d6c56b5ed48d25645696572ff
jenohara <jenny.s.ohara@gmail.com>
2020-01-25 16:48:40 +0000

    added index, new and create to mood controller

--- app/controllers/moods_controller.rb
+++ app/controllers/moods_controller.rb
@@ -1,2 +1,31 @@
 class MoodsController < ApplicationController
+
+    def index
+        @moods = Mood.all
+    end
+
+    def new
+        @mood = Mood.new
+    end
+
+    def create
+        @mood = Mood.new(mood_params)
+        if @mood.save
+            redirect_to :index 
+        else
+            render :index
+        end
+    end
+
+    def edit
+    end
+
+    def create
+    end
+
+    private
+
+    def mood_params
+        params.require(:mood).permit(:mood_type)
+    end
 end
