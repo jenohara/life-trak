ccd069753d291201242720b4bc314974eb378446
jenohara <jenny.s.ohara@gmail.com>
2020-02-06 13:18:48 +0000

    Updated validations to Journal

--- app/models/journal.rb
+++ app/models/journal.rb
@@ -1,9 +1,8 @@
 class Journal < ApplicationRecord
-    validates :title, presence: true
-
+    validates :title, presence: true, uniqueness: {:scope => [:user_id]}
     belongs_to :user
     
-    has_many :entries
+    has_many :entries, dependent: :destroy
     has_many :moods, :through => :entries
     accepts_nested_attributes_for :entries
 end
