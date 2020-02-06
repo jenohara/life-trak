09a04921e1942bd2a920cb4436281f7925a52b3e
jenohara <jenny.s.ohara@gmail.com>
2020-02-06 13:17:41 +0000

    Removed unncessary methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,24 +1,6 @@
 class ApplicationController < ActionController::Base
     before_action :configure_permitted_parameters, if: :devise_controller?
-    #before_action :authenticate_user! 
-    #before_action :verified_user
-    #helper_method :current_user
-  
-    #private
-  
-    # def verified_user
-    #   redirect_to '/' unless user_is_authenticated
-    # end
-  
-    # def user_is_authenticated
-    #   !!current_user
-    # end
-  
-    # def current_user
-    #   @current_user ||= User.find_by(id: session[:user_id])
-    # end
-    
-    #protected
+    #before_action :authenticate_user!, except: [:welcome, :home]
 
     def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :email, :password, :password_confirmation])
